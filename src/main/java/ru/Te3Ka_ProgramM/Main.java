e9b20600c33f30a73628eb315dabc43dc8a06310
Te3Ka <Mnement4813@yandex.ru>
2024-01-14 12:22:38 +0300

    Созданы константы для управления программой. Создана приватная переменная счётчика и get и set к ней. Тестовый запуск для проверки работоспособности программы.

--- src/main/java/ru/Te3Ka_ProgramM/Main.java
+++ src/main/java/ru/Te3Ka_ProgramM/Main.java
@@ -1,7 +1,27 @@
 package ru.Te3Ka_ProgramM;
-public class Main {
-    public static void main(String[] args) {
 
-        }
+import java.util.Scanner;
+
+public class Main {
+    // Константы команд для управления программой через консоль.
+    String PLUS_ITERATOR = "/inc";
+    String RESET_ITERATOR = "/reset";
+    String STOP_ITERATOR = "/stop";
+
+    Scanner scanner = new Scanner(System.in);
+    // Счётчик и методы для его изменения и получения данных
+    private int iterator;
+
+    void setIterator(int iterator) {
+        this.iterator = iterator;
+    }
+
+    int getIterator() {
+        return iterator;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Begin program");
     }
 }
