6c511f628dfa52424c4f12bd056bd647378b94cc
Te3Ka <Mnement4813@yandex.ru>
2024-01-14 12:40:07 +0300

    class Decorator Создан класс Decorator и несколько методов для написания текста в консоль на русском языке.
    Также итератор перенемён в отдельный класс Iterator

--- src/main/java/ru/Te3Ka_ProgramM/Main.java
+++ src/main/java/ru/Te3Ka_ProgramM/Main.java
@@ -2,6 +2,11 @@ package ru.Te3Ka_ProgramM;
 
 import java.util.Scanner;
 
+//***********************//
+//******Te3Ka_PaynE******//
+//*79811131773@yandex.ru*//
+//***********************//
+
 public class Main {
     // Константы команд для управления программой через консоль.
     String PLUS_ITERATOR = "/inc";
@@ -10,7 +15,38 @@ public class Main {
 
     Scanner scanner = new Scanner(System.in);
     // Счётчик и методы для его изменения и получения данных
-    private int iterator;
+
+    public static void main(String[] args) {
+        Decorator decorator = new Decorator();
+        decorator.startProgram();
+        decorator.showIterator();
+        decorator.author();
+    }
+}
+
+class Decorator {
+    void startProgram() {
+        System.out.println("Программа показывает инкрементируемый счётчик.");
+        System.out.println("При выходе из приложения показатель счётчика сохраняется.");
+        System.out.println("При повторном запуске счётчик загружается с последнего сохранённого положения.");
+        System.out.println();
+    }
+    void author() {
+        System.out.println();
+        System.out.println("/" + "*".repeat(23) + "/");
+        System.out.println("/******Te3Ka_PaynE******/");
+        System.out.println("/*79811131773@yandex.ru*/");
+        System.out.println("/" + "*".repeat(23) + "/");
+    }
+
+    void showIterator() {
+        Iterator iter = new Iterator();
+        System.out.println("Текущее значение счётчика: " + iter.getIterator());
+    }
+}
+
+class Iterator {
+    private int iterator = 0;
 
     void setIterator(int iterator) {
         this.iterator = iterator;
@@ -18,10 +54,5 @@ public class Main {
 
     int getIterator() {
         return iterator;
-    }
-
-
-    public static void main(String[] args) {
-        System.out.println("Begin program");
     }
 }
